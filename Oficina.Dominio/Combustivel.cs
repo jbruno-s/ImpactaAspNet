76a377095596c7e2caabce823b44de5ddee26073
Bruno Silva <jbruno_s@hotmail.com>
2018-11-26 22:31:59 -0200

    Dominio Repositorio

---
+++ Oficina.Dominio/Combustivel.cs
@@ -0,0 +1,12 @@
+﻿namespace Oficina.Dominio
+{
+    public enum Combustivel
+    {
+        Gasolina = 1,
+        Alcool = 2,
+        Flex,
+        Diesel = 4,
+        Eletrico = 5,
+        GNV = 6        
+    }
+}
